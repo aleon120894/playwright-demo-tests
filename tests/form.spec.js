6fb0ca004939d65385a761d5bc67d197eff81f95
aleon120894 <aleon120894@gmail.com>
2025-11-12 15:59:38 +0200

    Add CI actions to project

--- tests/form.spec.js
+++ tests/form.spec.js
@@ -5,9 +5,11 @@ test.describe('Form authentication', () => {
   
   test('checkboxes interaction', async ({ page }) => {
     
+    // Go to checkboxes checking
     await page.goto('https://the-internet.herokuapp.com/checkboxes');
     const firstCheckbox = page.locator('input[type="checkbox"]').first();
     
+    // Checkbox checking
     await firstCheckbox.check();
     await expect(firstCheckbox).toBeChecked();
   });
